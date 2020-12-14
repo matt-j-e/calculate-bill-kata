1a2beb85a3b56da7308aebb803fac469e722a529
mattedwards <the.matt.edwards@googlemail.com>
2020-12-14 22:09:54 +0000

    typo in the instructions. index.html should have read index.js. It worked when I put the code in that file and passed the tests

---
+++ index.js
@@ -0,0 +1,12 @@
+const calculateBill = (price, vat, tip) => {
+
+    if (typeof(price, vat, tip) === 'number') {
+        return "£" + (price + (price * vat /100) + tip);
+    } else {
+        return "error"
+    }
+
+};
+
+
+module.exports = calculateBill;
